bf4f523e50fe06a36606ff234f3daeb464ba0f9f
LibraryofLegends <tlorenz.iphone12@gmail.com>
2026-04-23 14:03:16 +0200

    Index.js erstellen

---
+++ Index.js
@@ -0,0 +1,15 @@
+const fetch = global.fetch || require("node-fetch");
+const express = require("express");
+const fs = require("fs");
+
+const app = express();
+app.use(express.json());
+
+const TOKEN = process.env.TOKEN;
+const TMDB_KEY = process.env.TMDB_KEY;
+const CHANNEL_ID = process.env.CHANNEL_ID;
+const BOT_USERNAME = process.env.BOT_USERNAME || "LIBRARY_OF_LEGENDS_Bot";
+
+const DB_FILE = "films.json";
+const HISTORY_FILE = "history.json";
+const SERIES_DB_FILE = "series.json";
