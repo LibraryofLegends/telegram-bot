bfe1e3399a4515c5158763a5401118554dc1fe8d
LibraryofLegends <tlorenz.iphone12@gmail.com>
2026-04-23 18:51:14 +0200

    Index.js aktualisieren

--- Index.js
+++ Index.js
@@ -13,3 +13,20 @@ const BOT_USERNAME = process.env.BOT_USERNAME || "LIBRARY_OF_LEGENDS_Bot";
 const DB_FILE = "films.json";
 const HISTORY_FILE = "history.json";
 const SERIES_DB_FILE = "series.json";
+
+// ================= DB =================
+function loadDB() {
+  if (!fs.existsSync(DB_FILE)) return [];
+  try {
+    return JSON.parse(fs.readFileSync(DB_FILE, "utf8") || "[]");
+  } catch {
+    return []; // 🔥 verhindert Crash bei kaputter JSON
+  }
+}
+
+let CACHE = loadDB();
+
+function saveDB(data) {
+  CACHE = data;
+  fs.writeFileSync(DB_FILE, JSON.stringify(data, null, 2));
+}
