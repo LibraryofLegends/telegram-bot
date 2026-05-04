e87c464f9bda8b7e97a2cfb230a905766e5eedb6
LibraryofLegends <tlorenz.iphone12@gmail.com>
2026-05-04 16:06:14 +0200

    env.js aktualisieren

--- config/env.js
+++ config/env.js
@@ -1,1 +1,19 @@
-(
+require("dotenv").config();
+
+function getEnv(key, fallback = null) {
+  if (process.env[key]) return process.env[key];
+  if (fallback !== null) return fallback;
+
+  console.warn(`⚠️ ENV missing: ${key}`);
+  return null;
+}
+
+module.exports = {
+  TOKEN: getEnv("TOKEN"),
+  BOT_USERNAME: getEnv("BOT_USERNAME"),
+
+  MAIN_CHANNEL_ID: getEnv("MAIN_CHANNEL_ID"),
+  SERIES_GROUP_ID: getEnv("SERIES_GROUP_ID"),
+
+  LOG_LEVEL: getEnv("LOG_LEVEL", "dev")
+};
