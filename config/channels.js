4ecb02086ea3c2d44f8c5538f14faf9280ab5377
LibraryofLegends <tlorenz.iphone12@gmail.com>
2026-05-04 16:07:15 +0200

    channels.js aktualisieren

--- config/channels.js
+++ config/channels.js
@@ -1,1 +1,16 @@
-(
+const { MAIN_CHANNEL_ID } = require("./env");
+
+// später erweiterbar (z.B. mehrere Channels)
+const CHANNELS = {
+  default: MAIN_CHANNEL_ID
+};
+
+function getTargetChannel(genres = []) {
+  // aktuell alles auf main
+  return CHANNELS.default;
+}
+
+module.exports = {
+  getTargetChannel,
+  CHANNELS
+};
