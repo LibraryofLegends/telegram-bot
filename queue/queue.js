5d2653b13cd4f042eafe56e8028b472435fd0d04
LibraryofLegends <tlorenz.iphone12@gmail.com>
2026-05-04 12:35:46 +0200

    queue.js aktualisieren

--- queue/queue.js
+++ queue/queue.js
@@ -1,1 +1,13 @@
-(
+const EventEmitter = require("events");
+
+class Queue extends EventEmitter {
+  constructor() {
+    super();
+  }
+
+  add(event, data) {
+    this.emit(event, data);
+  }
+}
+
+module.exports = new Queue();
