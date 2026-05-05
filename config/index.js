2d66047a6677fbcda93748d51c5e459b982d5676
LibraryofLegends <tlorenz.iphone12@gmail.com>
2026-05-05 14:15:23 +0200

    index.js aktualisieren

--- config/index.js
+++ config/index.js
@@ -1,1 +1,11 @@
-(
+const env = require("./env");
+const channels = require("./channels");
+const threads = require("./threads");
+const genres = require("./genres");
+
+module.exports = {
+  ...env,
+  ...channels,
+  ...threads,
+  ...genres
+};
