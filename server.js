879e7eec73faea6d55c98aee589998aa6b147669
LibraryofLegends <tlorenz.iphone12@gmail.com>
2026-05-08 06:13:21 +0200

    server.js aktualisieren

--- server.js
+++ server.js
@@ -1425,12 +1425,3 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`✅ Server läuft auf Port ${PORT}`);
 });
-
-// =============================
-// SERVER START
-// =============================
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`✅ Server läuft auf Port ${PORT}`);
-});
