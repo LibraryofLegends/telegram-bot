9f6bff2db907aa2c043eb15e84fd96963e600c44
LibraryofLegends <tlorenz.iphone12@gmail.com>
2026-05-13 08:10:57 +0200

    server.js aktualisieren

--- server.js
+++ server.js
@@ -348,6 +348,14 @@ async function createOrUpdateCollectionHub(tmdb, topicId) {
   if (hub?.message_id) {
     saveCollectionHubMessageId(tmdb.collectionId, hub.message_id);
   }
+  
+  if (hub?.message_id) {
+  await tg("pinChatMessage", {
+    chat_id: MOVIE_GROUP_ID,
+    message_id: hub.message_id,
+    disable_notification: true
+  });
+}
 
   return hub;
 }
