54907681dd98a19444482d4884b792c4aba49760
LibraryofLegends <tlorenz.iphone12@gmail.com>
2026-05-24 01:11:59 +0200

    server.js aktualisieren

--- server.js
+++ server.js
@@ -4367,21 +4367,21 @@ const seriesBanners = {
 };
 
 const genreBanners = {
-  Action: "DEIN_ACTION_BANNER_LINK",
-  Abenteuer: "DEIN_ABENTEUER_BANNER_LINK",
-  Komödie: "DEIN_KOMÖDIE_BANNER_LINK",
+  Action: null,
+  Abenteuer: null,
+  Komödie: null,
   Drama: null,
-  Familie: "DEIN_FAMILIE_BANNER_LINK",
-  Fantasy: "DEIN_FANTASY_BANNER_LINK",
-  Krimi: "DEIN_KRIMI_BANNER_LINK",
-  Horror: "DEIN_HORROR_BANNER_LINK",
-  Thriller: "DEIN_THRILLER_BANNER_LINK",
-  Mystery: "DEIN_MYSTERY_BANNER_LINK",
-  "Science Fiction": "DEIN_SCIENCE_FICTION_BANNER_LINK",
-  Kriegsfilme: "DEIN_KRIEGSFILME_BANNER_LINK",
-  Dokumentarfilme: "DEIN_DOKUMENTARFILME_BANNER_LINK",
-  Historie: "DEIN_HISTORIE_BANNER_LINK",
-  Liebesfilme: "DEIN_LIEBESFILME_BANNER_LINK"
+  Familie: null,
+  Fantasy: null,
+  Krimi: null,
+  Horror: null,
+  Thriller: null,
+  Mystery: null,
+  "Science Fiction": null,
+  Kriegsfilme: null,
+  Dokumentarfilme: null,
+  Historie: null,
+  Liebesfilme: null
 };
 
 const genreThemes = {
