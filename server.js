63236a54def6741fd2bb49742cc86656b2b1be3f
LibraryofLegends <tlorenz.iphone12@gmail.com>
2026-06-04 01:59:54 +0200

    server.js aktualisieren

--- server.js
+++ server.js
@@ -4702,8 +4702,8 @@ return (
     `${nexus.line3 ? `${nexus.line3}\n` : ""}` +
     `${nexus.line4 || "🌍 Timeline • Verified"}\n` +
     "━━━━━━━━━━━━━━━━━━\n" +
-    `${genreTags}\n` +
-    "@LibraryOfLegends"
+    "━━━━━━━━━━━━━━━━━━\n" +
+"@LibraryOfLegends"
   ).slice(0, 4000);
 }
 
