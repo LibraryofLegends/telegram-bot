475e7cd2347e695bf78ab90debc0ad8868a6c47c
LibraryofLegends <tlorenz.iphone12@gmail.com>
2026-07-24 00:54:40 +0200

    userbot-importer.js aktualisieren

--- userbot-importer.js
+++ userbot-importer.js
@@ -46,6 +46,10 @@ const {
     findOrCreateSeries,
 } = require("./database/repositories/series");
 
+const {
+    findOrCreateMovie,
+} = require("./database/repositories/movies");
+
 const apiId = Number(process.env.TELEGRAM_API_ID);
 const apiHash = process.env.TELEGRAM_API_HASH;
 const session = process.env.USERBOT_SESSION;
@@ -723,136 +727,6 @@ return firstEpisode;
 
 }
 
-async function findOrCreateMovie(
-    parsed,
-    tmdbData,
-    stagingMessageId = null
-) {
-
-    if (!pgPool)
-        return null;
-
-    if (!tmdbData)
-        return null;
-
-    let result = await pgPool.query(
-
-        `
-        SELECT *
-        FROM movies
-        WHERE tmdb_id=$1
-        LIMIT 1
-        `,
-
-        [
-
-            tmdbData.tmdbId
-
-        ]
-
-    );
-
-    if (result.rows.length) {
-
-        return result.rows[0];
-
-    }
-
-    result = await pgPool.query(
-
-        `
-        INSERT INTO movies(
-
-            title,
-            original_title,
-            year,
-
-            tmdb_id,
-            imdb_id,
-
-            overview,
-
-            poster_path,
-            backdrop_path,
-
-            vote_average,
-            vote_count,
-
-            runtime,
-
-            genres,
-
-            quality,
-            source,
-            codec,
-            audio,
-
-            staging_message_id
-
-        )
-
-        VALUES(
-
-            $1,$2,$3,
-            $4,$5,
-            $6,
-            $7,$8,
-            $9,$10,
-            $11,
-            $12,
-            $13,$14,$15,$16,
-            $17
-
-        )
-
-        RETURNING *
-
-        `,
-
-        [
-
-            tmdbData.title,
-
-            tmdbData.originalTitle,
-
-            parsed.year,
-
-            tmdbData.tmdbId,
-
-            tmdbData.imdbId,
-
-            tmdbData.overview,
-
-            tmdbData.poster,
-
-            tmdbData.backdrop,
-
-            tmdbData.voteAverage,
-
-            tmdbData.voteCount,
-
-            tmdbData.runtime,
-
-            tmdbData.genres,
-
-            parsed.quality,
-
-            parsed.source,
-
-            parsed.codec,
-
-            parsed.audio,
-
-            stagingMessageId
-
-        ]
-
-    );
-
-    return result.rows[0];
-
-}
-
 async function startUserbotImporter() {
   if (!isUserbotEnabled()) {
     console.log("ℹ️ Userbot Importer deaktiviert. USERBOT_ENABLED ist nicht true.");
@@ -979,9 +853,10 @@ let libraryMovie = null;
 if (parsed.type === "movie") {
 
     libraryMovie = await findOrCreateMovie(
-        parsed,
-        tmdbData
-    );
+    pgPool,
+    parsed,
+    tmdbData
+);
 
 }
 
