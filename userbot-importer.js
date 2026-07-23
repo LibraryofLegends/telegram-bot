3cb0cc9a7e5083a8762a84707c9efff778378b46
LibraryofLegends <tlorenz.iphone12@gmail.com>
2026-07-24 00:49:03 +0200

    userbot-importer.js aktualisieren

--- userbot-importer.js
+++ userbot-importer.js
@@ -42,6 +42,10 @@ const {
     findOrCreateSeason,
 } = require("./database/repositories/seasons");
 
+const {
+    findOrCreateSeries,
+} = require("./database/repositories/series");
+
 const apiId = Number(process.env.TELEGRAM_API_ID);
 const apiHash = process.env.TELEGRAM_API_HASH;
 const session = process.env.USERBOT_SESSION;
@@ -496,138 +500,6 @@ async function getTMDBDetails(type, tmdbId) {
         return null;
 
     }
-
-}
-
-// =========================================================
-// Serienverwaltung
-// =========================================================
-
-async function findOrCreateSeries(
-
-    parsed,
-
-    tmdbData = null
-
-) {
-
-    if (!pgPool)
-
-        return null;
-
-    let result = await pgPool.query(
-
-        `
-
-        SELECT *
-
-        FROM series
-
-        WHERE LOWER(title)=LOWER($1)
-
-        LIMIT 1
-
-        `,
-
-        [
-
-            parsed.title
-
-        ]
-
-    );
-
-    if (result.rows.length) {
-
-        return result.rows[0];
-
-    }
-
-    result = await pgPool.query(
-
-    `
-
-    INSERT INTO series(
-
-    title,
-    original_title,
-    year,
-    tmdb_id,
-    imdb_id,
-
-    overview,
-    poster_path,
-    backdrop_path,
-
-    vote_average,
-    vote_count,
-
-    genres,
-
-    number_of_seasons,
-    number_of_episodes
-
-)
-
-    VALUES(
-
-    $1,
-    $2,
-    $3,
-    $4,
-    $5,
-
-    $6,
-    $7,
-    $8,
-
-    $9,
-    $10,
-
-    $11,
-
-    $12,
-    $13
-
-)
-
-    RETURNING *
-
-    `,
-
-    [
-    tmdbData?.title || parsed.title,
-
-    tmdbData?.originalTitle || null,
-
-    parsed.year || null,
-
-    tmdbData?.tmdbId || null,
-
-    tmdbData?.imdbId || null,
-
-    tmdbData?.overview || null,
-
-    tmdbData?.poster || null,
-
-    tmdbData?.backdrop || null,
-
-    tmdbData?.voteAverage || null,
-
-    tmdbData?.voteCount || null,
-
-    tmdbData?.genres || [],
-
-    tmdbData?.seasons || null,
-
-    tmdbData?.episodes || null
-]
-
-);
-
-    console.log("✅ Neue Serie angelegt:", parsed.title);
-
-    return result.rows[0];
 
 }
 
@@ -1116,9 +988,10 @@ if (parsed.type === "movie") {
 else if (parsed.type === "series") {
 
     librarySeries = await findOrCreateSeries(
-        parsed,
-        tmdbData
-    );
+    pgPool,
+    parsed,
+    tmdbData
+);
 
     librarySeason = await findOrCreateSeason(
     pgPool,
