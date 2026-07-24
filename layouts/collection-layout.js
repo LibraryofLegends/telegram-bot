09b48b42b4dcd1eaeb1d209338f324ede1280aec
LibraryofLegends <tlorenz.iphone12@gmail.com>
2026-07-24 18:09:36 +0200

    collection-layout.js erstellen

---
+++ layouts/collection-layout.js
@@ -0,0 +1,36 @@
+// ======================================================
+// COLLECTION LAYOUT
+// ======================================================
+//
+// Layouts für Collections
+//
+// Enthält:
+// - collectionSagaCaption()
+//
+// ======================================================
+
+
+
+// ======================================================
+// COLLECTION SAGA CAPTION
+// ======================================================
+
+async function collectionSagaCaption(movie) {
+
+    // <<< HIER KOMMT DEINE KOMPLETTE
+    // collectionSagaCaption()
+    // UNVERÄNDERT hinein >>>
+
+}
+
+
+
+// ======================================================
+// EXPORTS
+// ======================================================
+
+module.exports = {
+
+    collectionSagaCaption
+
+};
