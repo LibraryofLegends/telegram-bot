0c004400b179230ac74f49646f6d89596a8b2f35
LibraryofLegends <tlorenz.iphone12@gmail.com>
2026-07-24 03:11:53 +0200

    library-sync.js aktualisieren

--- importer/library-sync.js
+++ importer/library-sync.js
@@ -2,73 +2,37 @@ async function syncLibrary(
     pgPool,
     parsed,
     tmdbData,
-    stagingMessageId,
     findOrCreateMovie,
     findOrCreateSeries,
-    findOrCreateSeason,
-    findOrCreateEpisode
+    findOrCreateSeason
 ) {
     let librarySeries = null;
     let librarySeason = null;
-    let libraryEpisode = null;
     let libraryMovie = null;
 
     if (parsed.type === "movie") {
-    libraryMovie = await findOrCreateMovie(
-        pgPool,
-        parsed,
-        tmdbData
-    );
-} else if (parsed.type === "series") {
-    librarySeries = await findOrCreateSeries(
-        pgPool,
-        parsed,
-        tmdbData
-    );
+        libraryMovie = await findOrCreateMovie(
+            pgPool,
+            parsed,
+            tmdbData
+        );
+    } else if (parsed.type === "series") {
+        librarySeries = await findOrCreateSeries(
+            pgPool,
+            parsed,
+            tmdbData
+        );
 
-    librarySeason = await findOrCreateSeason(
-        pgPool,
-        librarySeries.id,
-        parsed.season
-    );
-}
-
-if (parsed.type === "series" && librarySeason) {
-    libraryEpisode = await findOrCreateEpisode(
-        pgPool,
-        librarySeason.id,
-        parsed,
-        stagingMessageId
-    );
-}
-
-if (
-    parsed.type === "movie" &&
-    libraryMovie
-) {
-    await pgPool.query(
-        `
-        UPDATE movies
-        SET
-            staging_message_id=$1,
-            updated_at=NOW()
-        WHERE id=$2
-        `,
-        [
-            stagingMessageId,
-            libraryMovie.id
-        ]
-    );
-}
+        librarySeason = await findOrCreateSeason(
+            pgPool,
+            librarySeries.id,
+            parsed.season
+        );
+    }
 
     return {
         librarySeries,
         librarySeason,
-        libraryEpisode,
         libraryMovie,
     };
 }
-
-module.exports = {
-    syncLibrary,
-};
