4a2c530c27bcdc1158e5bb8a283ce6e887f562f7
LibraryofLegends <tlorenz.iphone12@gmail.com>
2026-07-30 11:16:40 +0200

    ModuleQueueStore.js erstellen

---
+++ src/core/ModuleQueueStore.js
@@ -0,0 +1,57 @@
+'use strict';
+
+class ModuleQueueStore {
+
+    constructor() {
+
+        this.store = new Map();
+
+    }
+
+    save(name, queue) {
+
+        this.store.set(name, queue);
+
+        return this;
+
+    }
+
+    load(name) {
+
+        return this.store.get(name) || null;
+
+    }
+
+    has(name) {
+
+        return this.store.has(name);
+
+    }
+
+    delete(name) {
+
+        return this.store.delete(name);
+
+    }
+
+    clear() {
+
+        this.store.clear();
+
+    }
+
+    keys() {
+
+        return [...this.store.keys()];
+
+    }
+
+    values() {
+
+        return [...this.store.values()];
+
+    }
+
+}
+
+module.exports = ModuleQueueStore;
