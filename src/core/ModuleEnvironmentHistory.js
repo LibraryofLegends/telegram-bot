68ddbb2bbd4f1372a1b2170c0950003bece74e70
LibraryofLegends <tlorenz.iphone12@gmail.com>
2026-07-30 10:50:00 +0200

    ModuleEnvironmentHistory.js erstellen

---
+++ src/core/ModuleEnvironmentHistory.js
@@ -0,0 +1,57 @@
+'use strict';
+
+class ModuleEnvironmentHistory {
+
+    constructor() {
+
+        this.history = [];
+
+    }
+
+    add(name, active) {
+
+        this.history.push({
+
+            name,
+            active,
+            timestamp: new Date()
+
+        });
+
+        return this;
+
+    }
+
+    latest() {
+
+        return this.history.at(-1) || null;
+
+    }
+
+    first() {
+
+        return this.history[0] || null;
+
+    }
+
+    all() {
+
+        return [...this.history];
+
+    }
+
+    clear() {
+
+        this.history = [];
+
+    }
+
+    count() {
+
+        return this.history.length;
+
+    }
+
+}
+
+module.exports = ModuleEnvironmentHistory;
