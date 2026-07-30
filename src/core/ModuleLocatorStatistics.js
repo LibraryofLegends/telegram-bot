1ecd5ff39cc60992a2de3a2c605d3c7df5503bbb
LibraryofLegends <tlorenz.iphone12@gmail.com>
2026-07-30 13:38:35 +0200

    ModuleLocatorStatistics.js erstellen

---
+++ src/core/ModuleLocatorStatistics.js
@@ -0,0 +1,29 @@
+'use strict';
+
+class ModuleLocatorStatistics {
+
+    constructor(registry) {
+
+        this.registry = registry;
+
+    }
+
+    total() {
+
+        return this.registry.count();
+
+    }
+
+    toJSON() {
+
+        return {
+
+            total: this.total()
+
+        };
+
+    }
+
+}
+
+module.exports = ModuleLocatorStatistics;
