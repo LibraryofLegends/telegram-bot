e8319721ded8ab27d1d3c11dd7ccb2e84a2deef5
LibraryofLegends <tlorenz.iphone12@gmail.com>
2026-07-30 11:30:29 +0200

    ModuleStackStatistics.js erstellen

---
+++ src/core/ModuleStackStatistics.js
@@ -0,0 +1,29 @@
+'use strict';
+
+class ModuleStackStatistics {
+
+    constructor(registry) {
+
+        this.registry = registry;
+
+    }
+
+    total() {
+
+        return this.registry.count();
+
+    }
+
+    toJSON() {
+
+        return {
+
+            total: this.total()
+
+        };
+
+    }
+
+}
+
+module.exports = ModuleStackStatistics;
