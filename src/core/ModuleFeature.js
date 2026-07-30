7daba12b3e0adec80652d126787334da15f99cc5
LibraryofLegends <tlorenz.iphone12@gmail.com>
2026-07-30 08:58:30 +0200

    ModuleFeature.js erstellen

---
+++ src/core/ModuleFeature.js
@@ -0,0 +1,61 @@
+'use strict';
+
+class ModuleFeature {
+
+    constructor(name, enabled = true) {
+
+        this.name = name;
+        this.enabled = enabled;
+
+    }
+
+    getName() {
+
+        return this.name;
+
+    }
+
+    isEnabled() {
+
+        return this.enabled;
+
+    }
+
+    enable() {
+
+        this.enabled = true;
+
+        return this;
+
+    }
+
+    disable() {
+
+        this.enabled = false;
+
+        return this;
+
+    }
+
+    toggle() {
+
+        this.enabled = !this.enabled;
+
+        return this;
+
+    }
+
+    toJSON() {
+
+        return {
+
+            name: this.name,
+            enabled: this.enabled
+
+        };
+
+    }
+
+}
+
+module.exports = ModuleFeature;
