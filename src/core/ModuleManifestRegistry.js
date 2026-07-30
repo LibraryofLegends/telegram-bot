cba34bd9960c9e81eb49cf24ee6ca1b988e492d3
LibraryofLegends <tlorenz.iphone12@gmail.com>
2026-07-30 15:20:29 +0200

    ModuleManifestRegistry.js erstellen

---
+++ src/core/ModuleManifestRegistry.js
@@ -0,0 +1,63 @@
+'use strict';
+
+class ModuleManifestRegistry {
+
+    constructor() {
+
+        this.items = new Map();
+
+    }
+
+    register(manifest) {
+
+        this.items.set(
+
+            manifest.getName(),
+
+            manifest
+
+        );
+
+        return this;
+
+    }
+
+    get(name) {
+
+        return this.items.get(name) || null;
+
+    }
+
+    has(name) {
+
+        return this.items.has(name);
+
+    }
+
+    remove(name) {
+
+        return this.items.delete(name);
+
+    }
+
+    all() {
+
+        return [...this.items.values()];
+
+    }
+
+    clear() {
+
+        this.items.clear();
+
+    }
+
+    count() {
+
+        return this.items.size;
+
+    }
+
+}
+
+module.exports = ModuleManifestRegistry;
