b225c3f803a617187c0ecb1742e991aca3811304
LibraryofLegends <tlorenz.iphone12@gmail.com>
2026-07-30 11:43:43 +0200

    ModuleCacheStatistics.js erstellen

---
+++ src/core/ModuleCacheStatistics.js
@@ -0,0 +1,29 @@
+'use strict';
+
+class ModuleCacheStatistics {
+
+    constructor(registry) {
+
+        this.registry = registry;
+
+    }
+
+    total() {
+
+        return this.registry.count();
+
+    }
+
+    toJSON() {
+
+        return {
+
+            total: this.total()
+
+        };
+
+    }
+
+}
+
+module.exports = ModuleCacheStatistics;
