8bfad885e70f952abaafafc3f491747f5420dfd5
LibraryofLegends <tlorenz.iphone12@gmail.com>
2026-07-30 13:53:23 +0200

    ModuleResolverHistory.js erstellen

---
+++ src/core/ModuleResolverHistory.js
@@ -0,0 +1,50 @@
+'use strict';
+
+class ModuleResolverHistory {
+
+    constructor() {
+
+        this.history = [];
+
+    }
+
+    add(entries) {
+
+        this.history.push({
+
+            entries,
+            timestamp: new Date()
+
+        });
+
+        return this;
+
+    }
+
+    latest() {
+
+        return this.history.at(-1) || null;
+
+    }
+
+    all() {
+
+        return [...this.history];
+
+    }
+
+    clear() {
+
+        this.history = [];
+
+    }
+
+    count() {
+
+        return this.history.length;
+
+    }
+
+}
+
+module.exports = ModuleResolverHistory;
