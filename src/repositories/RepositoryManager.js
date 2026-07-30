2cc0abce78da4a0a93cdcafc1ee85d9f4d7c3d87
LibraryofLegends <tlorenz.iphone12@gmail.com>
2026-07-30 02:52:25 +0200

    RepositoryManager.js aktualisieren

--- src/repositories/RepositoryManager.js
+++ src/repositories/RepositoryManager.js
@@ -7,13 +7,21 @@
  * ------------------------------------------------------------------------
  * Beschreibung:
  *
- * Zentrale Verwaltung aller Repositories.
+ * Zentrale Verwaltung aller Repository-Klassen.
  *
- * Diese Klasse erstellt sämtliche Repository-Instanzen
- * und stellt sie zentral zur Verfügung.
+ * Erstellt sämtliche Repository-Instanzen und stellt sie
+ * zentral innerhalb der Anwendung zur Verfügung.
+ *
+ * Funktionen:
+ * - Initialisierung aller Repositories
+ * - Zentrale Verwaltung
+ * - Dynamisches Registrieren
+ * - Repository-Lookup
+ * - Repository-Informationen
+ * - Reset aller Repositories
  *
  * Version:
- * 1.0.0
+ * 2.0.0
  * ========================================================================
  */
 
@@ -47,43 +55,140 @@ class RepositoryManager {
 
     constructor(db) {
 
+        if (!db) {
+
+            throw new Error('RepositoryManager benötigt eine Datenbankinstanz.');
+
+        }
+
         this.db = db;
 
+        this.repositories = {};
+
+        this.initialize();
+
+    }
+
+    /**
+     * ============================================================
+     * Initialisierung
+     * ============================================================
+     */
+
+    initialize() {
+
         /**
-         * ========================================================
+         * --------------------------------------------------------
          * Medien
-         * ========================================================
+         * --------------------------------------------------------
          */
 
-        this.movies = new MovieRepository(db);
-        this.series = new SeriesRepository(db);
-        this.seasons = new SeasonRepository(db);
-        this.episodes = new EpisodeRepository(db);
+        this.register('movies', new MovieRepository(this.db));
+        this.register('series', new SeriesRepository(this.db));
+        this.register('seasons', new SeasonRepository(this.db));
+        this.register('episodes', new EpisodeRepository(this.db));
 
         /**
-         * ========================================================
+         * --------------------------------------------------------
          * Stammdaten
-         * ========================================================
+         * --------------------------------------------------------
          */
 
-        this.people = new PersonRepository(db);
-        this.genres = new GenreRepository(db);
-        this.collections = new CollectionRepository(db);
-        this.studios = new StudioRepository(db);
-        this.companies = new CompanyRepository(db);
-        this.countries = new CountryRepository(db);
-        this.languages = new LanguageRepository(db);
+        this.register('people', new PersonRepository(this.db));
+        this.register('genres', new GenreRepository(this.db));
+        this.register('collections', new CollectionRepository(this.db));
+        this.register('studios', new StudioRepository(this.db));
+        this.register('companies', new CompanyRepository(this.db));
+        this.register('countries', new CountryRepository(this.db));
+        this.register('languages', new LanguageRepository(this.db));
 
         /**
-         * ========================================================
+         * --------------------------------------------------------
          * System
-         * ========================================================
+         * --------------------------------------------------------
          */
 
-        this.files = new FileRepository(db);
-        this.users = new UserRepository(db);
-        this.workflows = new WorkflowRepository(db);
-        this.aiJobs = new AIJobRepository(db);
+        this.register('files', new FileRepository(this.db));
+        this.register('users', new UserRepository(this.db));
+        this.register('workflows', new WorkflowRepository(this.db));
+        this.register('aiJobs', new AIJobRepository(this.db));
+
+    }
+
+    /**
+     * ============================================================
+     * Registrierung
+     * ============================================================
+     */
+
+    register(name, repository) {
+
+        if (typeof name !== 'string' || !name.trim()) {
+
+            throw new Error('Ungültiger Repository-Name.');
+
+        }
+
+        if (!repository) {
+
+            throw new Error(`Repository "${name}" ist ungültig.`);
+
+        }
+
+        this.repositories[name] = repository;
+
+        this[name] = repository;
+
+        return this;
+
+    }
+
+    /**
+     * ============================================================
+     * Repository abrufen
+     * ============================================================
+     */
+
+    get(name) {
+
+        return this.repositories[name] ?? null;
+
+    }
+
+    /**
+     * ============================================================
+     * Repository vorhanden?
+     * ============================================================
+     */
+
+    has(name) {
+
+        return Object.prototype.hasOwnProperty.call(
+            this.repositories,
+            name
+        );
+
+    }
+
+    /**
+     * ============================================================
+     * Repository entfernen
+     * ============================================================
+     */
+
+    remove(name) {
+
+        if (!this.has(name)) {
+
+            return false;
+
+        }
+
+        delete this.repositories[name];
+
+        delete this[name];
+
+        return true;
 
     }
 
@@ -101,44 +206,65 @@ class RepositoryManager {
 
     /**
      * ============================================================
-     * Repository nach Name
-     * ============================================================
-     */
-
-    get(name) {
-
-        return this[name] ?? null;
-
-    }
-
-    /**
-     * ============================================================
-     * Prüfen
-     * ============================================================
-     */
-
-    has(name) {
-
-        return Object.prototype.hasOwnProperty.call(this, name);
-
-    }
-
-    /**
-     * ============================================================
-     * Repository-Liste
+     * Alle Repository-Namen
      * ============================================================
      */
 
     list() {
 
-        return Object.keys(this)
-            .filter(key => key !== 'db');
+        return Object.keys(this.repositories).sort();
 
     }
 
     /**
      * ============================================================
-     * Informationen
+     * Alle Repository-Objekte
+     * ============================================================
+     */
+
+    all() {
+
+        return Object.values(this.repositories);
+
+    }
+
+    /**
+     * ============================================================
+     * Anzahl
+     * ============================================================
+     */
+
+    count() {
+
+        return this.list().length;
+
+    }
+
+    /**
+     * ============================================================
+     * Alle Repositories zurücksetzen
+     * ============================================================
+     */
+
+    reset() {
+
+        for (const repository of this.all()) {
+
+            if (typeof repository.reset === 'function') {
+
+                repository.reset();
+
+            }
+
+        }
+
+        return this;
+
+    }
+
+    /**
+     * ============================================================
+     * Repository-Informationen
      * ============================================================
      */
 
@@ -148,9 +274,39 @@ class RepositoryManager {
 
             repositories: this.list(),
 
-            count: this.list().length
+            count: this.count(),
+
+            database: !!this.db,
+
+            repositoryInfo: this.all().map(repository => {
+
+                if (typeof repository.toJSON === 'function') {
+
+                    return repository.toJSON();
+
+                }
+
+                return {
+
+                    repository: repository.constructor.name
+
+                };
+
+            })
 
         };
+
+    }
+
+    /**
+     * ============================================================
+     * JSON
+     * ============================================================
+     */
+
+    toJSON() {
+
+        return this.info();
 
     }
 
