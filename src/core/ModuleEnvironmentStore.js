8b3741ce9d24f50417037116bdf2742fba1a4aaa
LibraryofLegends <tlorenz.iphone12@gmail.com>
2026-07-30 10:49:15 +0200

    ModuleEnvironmentStore.js erstellen

---
+++ src/core/ModuleEnvironmentStore.js
@@ -0,0 +1,57 @@
+'use strict';
+
+class ModuleEnvironmentStore {
+
+    constructor() {
+
+        this.store = new Map();
+
+    }
+
+    save(name, environment) {
+
+        this.store.set(name, environment);
+
+        return this;
+
+    }
+
+    load(name) {
+
+        return this.store.get(name) || null;
+
+    }
+
+    has(name) {
+
+        return this.store.has(name);
+
+    }
+
+    delete(name) {
+
+        return this.store.delete(name);
+
+    }
+
+    keys() {
+
+        return [...this.store.keys()];
+
+    }
+
+    values() {
+
+        return [...this.store.values()];
+
+    }
+
+    clear() {
+
+        this.store.clear();
+
+    }
+
+}
+
+module.exports = ModuleEnvironmentStore;
