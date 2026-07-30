6ac806ef57aa7b37566f7ace2aaf9966de1e58d2
LibraryofLegends <tlorenz.iphone12@gmail.com>
2026-07-30 08:29:06 +0200

    ModuleParameterStatistics.js erstellen

---
+++ src/core/ModuleParameterStatistics.js
@@ -0,0 +1,29 @@
+'use strict';
+
+class ModuleParameterStatistics {
+
+    constructor(registry) {
+
+        this.registry = registry;
+
+    }
+
+    total() {
+
+        return this.registry.count();
+
+    }
+
+    toJSON() {
+
+        return {
+
+            total: this.total()
+
+        };
+
+    }
+
+}
+
+module.exports = ModuleParameterStatistics;
