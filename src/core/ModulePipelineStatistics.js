89d94c48deb6bb4905019468ddac836b9a661f72
LibraryofLegends <tlorenz.iphone12@gmail.com>
2026-07-30 11:09:53 +0200

    ModulePipelineStatistics.js erstellen

---
+++ src/core/ModulePipelineStatistics.js
@@ -0,0 +1,29 @@
+'use strict';
+
+class ModulePipelineStatistics {
+
+    constructor(registry) {
+
+        this.registry = registry;
+
+    }
+
+    total() {
+
+        return this.registry.count();
+
+    }
+
+    toJSON() {
+
+        return {
+
+            total: this.total()
+
+        };
+
+    }
+
+}
+
+module.exports = ModulePipelineStatistics;
