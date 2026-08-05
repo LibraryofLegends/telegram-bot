239c7071f5bb085abb456dcec69a4d6f4724da00
LibraryofLegends <tlorenz.iphone12@gmail.com>
2026-08-05 08:13:32 +0200

    class.template.ts erstellen

---
+++ Library Of Legends/framework/templates/class.template.ts
@@ -0,0 +1,41 @@
+/*
+===============================================================================
+
+                    PROJECT PHOENIX
+
+===============================================================================
+
+Component...........: Component Name
+
+Architecture Layer..: Framework
+
+Module..............: Module Name
+
+LOL-ID..............: LOL-XXXX-0000
+
+File................: class-name.ts
+
+Location............:
+Library Of Legends/framework/module/
+
+Version.............: 1.0.0
+
+Status..............: Draft
+
+Description.........
+
+Describe the purpose of this class.
+
+===============================================================================
+*/
+
+export class ClassName {
+
+    /**
+     * Creates a new instance.
+     */
+    public constructor() {
+
+    }
+
+}
