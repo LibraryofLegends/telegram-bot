cbcff72ff0f951df7c9e75f5d6732a4f924e7af0
LibraryofLegends <tlorenz.iphone12@gmail.com>
2026-08-05 09:57:00 +0200

    feature.template.ts erstellen

---
+++ Library Of Legends/framework/templates/feature.template.ts
@@ -0,0 +1,48 @@
+/*
+===============================================================================
+
+                    PROJECT PHOENIX
+
+===============================================================================
+
+Feature.............: Feature Name
+
+Architecture Layer..: Feature
+
+Module..............: Module Name
+
+Feature-ID..........: FEATURE-000
+
+LOL-ID..............: LOL-FEATURE-0000
+
+File................: feature-name.ts
+
+Location............:
+Library Of Legends/features/module/
+
+Version.............: 1.0.0
+
+Status..............: Draft
+
+Description.........
+
+Represents the entry point of a business feature.
+
+Coordinates services, repositories and providers while keeping the
+business workflow isolated from the framework.
+
+===============================================================================
+*/
+
+export class FeatureName {
+
+    /**
+     * Executes the feature.
+     */
+    public async execute(): Promise<void> {
+
+        // TODO
+
+    }
+
+}
