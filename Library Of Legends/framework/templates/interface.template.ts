bd1ec063f808b04d57f1dd26e863b6009716da0e
LibraryofLegends <tlorenz.iphone12@gmail.com>
2026-08-05 08:20:15 +0200

    interface.template.ts erstellen

---
+++ Library Of Legends/framework/templates/interface.template.ts
@@ -0,0 +1,39 @@
+/*
+===============================================================================
+
+                    PROJECT PHOENIX
+
+===============================================================================
+
+Component...........: Interface Name
+
+Architecture Layer..: Framework
+
+Module..............: Module Name
+
+LOL-ID..............: LOL-XXXX-0000
+
+File................: interface-name.ts
+
+Location............:
+Library Of Legends/framework/module/
+
+Version.............: 1.0.0
+
+Status..............: Draft
+
+Description.........
+
+Defines the public contract for a component.
+
+===============================================================================
+*/
+
+export interface InterfaceName {
+
+    /**
+     * Unique identifier.
+     */
+    readonly id: string;
+
+}
