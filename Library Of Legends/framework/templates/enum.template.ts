f342d857191e255da691ec2afa9ab5e16afd2fd4
LibraryofLegends <tlorenz.iphone12@gmail.com>
2026-08-05 08:31:10 +0200

    enum.template.ts erstellen

---
+++ Library Of Legends/framework/templates/enum.template.ts
@@ -0,0 +1,39 @@
+/*
+===============================================================================
+
+                    PROJECT PHOENIX
+
+===============================================================================
+
+Component...........: Enumeration Name
+
+Architecture Layer..: Framework
+
+Module..............: Module Name
+
+LOL-ID..............: LOL-XXXX-0000
+
+File................: enum-name.ts
+
+Location............:
+Library Of Legends/framework/module/
+
+Version.............: 1.0.0
+
+Status..............: Draft
+
+Description.........
+
+Defines a strongly typed enumeration used throughout Project Phoenix.
+
+===============================================================================
+*/
+
+export enum EnumName {
+
+    /**
+     * Default value.
+     */
+    DEFAULT = "DEFAULT",
+
+}
