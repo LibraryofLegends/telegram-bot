69216ada78255940d9e58daf0be40399a1527d2d
LibraryofLegends <tlorenz.iphone12@gmail.com>
2026-08-07 14:58:57 +0200

    index.ts erstellen

---
+++ Library Of Legends/src/domain/catalog/franchise/index.ts
@@ -0,0 +1,38 @@
+/*
+===============================================================================
+
+                            PROJECT PHOENIX
+
+===============================================================================
+
+Component...........: Franchise Package
+
+Architecture Layer..: Domain
+
+Module..............: Catalog
+
+Module ID...........: LOL-MOD-FRA-0001
+
+LOL-ID..............: LOL-FRA-IDX-0001
+
+File................: index.ts
+
+Location............
+Library Of Legends/src/domain/catalog/franchise/
+
+Version.............: 1.0.0
+
+Status..............: Stable
+
+Lifecycle...........: Development
+
+Description.........
+
+Public exports for the Franchise domain package.
+
+===============================================================================
+*/
+
+export * from "./franchise";
+export * from "./franchise-id";
+export * from "./franchise-name";
