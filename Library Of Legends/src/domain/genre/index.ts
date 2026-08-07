a91cb148db1011aba93df6833f441d708354d932
LibraryofLegends <tlorenz.iphone12@gmail.com>
2026-08-07 14:09:26 +0200

    index.ts erstellen

---
+++ Library Of Legends/src/domain/genre/index.ts
@@ -0,0 +1,36 @@
+/*
+===============================================================================
+
+                            PROJECT PHOENIX
+
+===============================================================================
+
+Component...........: Genre Package
+
+Architecture Layer..: Domain
+
+Module..............: Genre
+
+Module ID...........: LOL-MOD-GEN-0001
+
+LOL-ID..............: LOL-GEN-IDX-0001
+
+File................: index.ts
+
+Location............
+Library Of Legends/src/domain/genre/
+
+Version.............: 1.0.0
+
+Status..............: Stable
+
+Lifecycle...........: Development
+
+Description.........
+
+Public exports for the Genre domain package.
+
+===============================================================================
+*/
+
+export * from "./genre";
