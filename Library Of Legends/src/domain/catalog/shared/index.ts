64f0c75933e609e5f8bb8c9c430c3a94cd30c3de
LibraryofLegends <tlorenz.iphone12@gmail.com>
2026-08-07 15:11:12 +0200

    index.ts erstellen

---
+++ Library Of Legends/src/domain/catalog/shared/index.ts
@@ -0,0 +1,43 @@
+/*
+===============================================================================
+
+                            PROJECT PHOENIX
+
+===============================================================================
+
+Component...........: Catalog Shared Package
+
+Architecture Layer..: Domain
+
+Module..............: Catalog Shared
+
+Module ID...........: LOL-MOD-CAT-0001
+
+LOL-ID..............: LOL-CAT-IDX-0001
+
+File................: index.ts
+
+Location............
+Library Of Legends/src/domain/catalog/shared/
+
+Version.............: 1.0.0
+
+Status..............: Stable
+
+Lifecycle...........: Development
+
+Description.........
+
+Public exports for the Catalog Shared Kernel.
+
+===============================================================================
+*/
+
+export * from "./catalog-item";
+export * from "./catalog-name";
+export * from "./catalog-description";
+
+export * from "./external-identifier";
+export * from "./external-identifier-set";
+
+export * from "./artwork";
