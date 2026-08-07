b07c06fecf5df8ab7f5bba3a22cb89eea76b610c
LibraryofLegends <tlorenz.iphone12@gmail.com>
2026-08-07 14:18:34 +0200

    index.ts erstellen

---
+++ Library Of Legends/src/domain/person/index.ts
@@ -0,0 +1,38 @@
+/*
+===============================================================================
+
+                            PROJECT PHOENIX
+
+===============================================================================
+
+Component...........: Person Package
+
+Architecture Layer..: Domain
+
+Module..............: Person
+
+Module ID...........: LOL-MOD-PER-0001
+
+LOL-ID..............: LOL-PER-IDX-0001
+
+File................: index.ts
+
+Location............
+Library Of Legends/src/domain/person/
+
+Version.............: 1.0.0
+
+Status..............: Stable
+
+Lifecycle...........: Development
+
+Description.........
+
+Public exports for the Person domain package.
+
+===============================================================================
+*/
+
+export * from "./person";
+export * from "./person-id";
+export * from "./person-name";
