5f7bb5cc8aa0fc5a86c90ec7e747d1136014cb30
LibraryofLegends <tlorenz.iphone12@gmail.com>
2026-08-07 15:17:32 +0200

    index.ts erstellen

---
+++ Library Of Legends/src/domain/catalog/studio/index.ts
@@ -0,0 +1,37 @@
+/*
+===============================================================================
+
+                            PROJECT PHOENIX
+
+===============================================================================
+
+Component...........: Studio Package
+
+Architecture Layer..: Domain
+
+Module..............: Catalog
+
+Module ID...........: LOL-MOD-STU-0001
+
+LOL-ID..............: LOL-STU-IDX-0001
+
+File................: index.ts
+
+Location............
+Library Of Legends/src/domain/catalog/studio/
+
+Version.............: 1.0.0
+
+Status..............: Stable
+
+Lifecycle...........: Development
+
+Description.........
+
+Public exports for the Studio domain package.
+
+===============================================================================
+*/
+
+export * from "./studio";
+export * from "./studio-id";
