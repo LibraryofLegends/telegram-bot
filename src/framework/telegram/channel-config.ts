da46a04d257f2d6338ad0ddc6fb7837886b18444
LibraryofLegends <tlorenz.iphone12@gmail.com>
2026-08-10 19:10:15 +0200

    channel-config.ts erstellen

---
+++ src/framework/telegram/channel-config.ts
@@ -0,0 +1,14 @@
+export const TELEGRAM_CHANNELS = {
+
+    action: "-100XXXXXXXXXX",
+    horror: "-100XXXXXXXXXX",
+    scifi: "-100XXXXXXXXXX",
+    drama: "-100XXXXXXXXXX",
+    comedy: "-100XXXXXXXXXX",
+    animation: "-100XXXXXXXXXX",
+    crime: "-100XXXXXXXXXX",
+    documentary: "-100XXXXXXXXXX",
+    kids: "-100XXXXXXXXXX",
+    general: "-100XXXXXXXXXX"
+
+};
