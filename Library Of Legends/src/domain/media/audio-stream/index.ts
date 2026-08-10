e17040c2dfdcb20a4e4c9194e3689ef8061ed5de
LibraryofLegends <tlorenz.iphone12@gmail.com>
2026-08-10 07:44:49 +0200

    index.ts erstellen

---
+++ Library Of Legends/src/domain/media/audio-stream/index.ts
@@ -0,0 +1,40 @@
+/*
+===============================================================================
+
+                            PROJECT PHOENIX
+
+===============================================================================
+
+Component...........: AudioStream Package
+
+Architecture Layer..: Domain
+
+Module..............: Media
+
+Module ID...........: LOL-MOD-AST-0001
+
+LOL-ID..............: LOL-AST-IDX-0001
+
+File................: index.ts
+
+Location............
+Library Of Legends/src/domain/media/audio-stream/
+
+Version.............: 1.0.0
+
+Status..............: Stable
+
+Lifecycle...........: Development
+
+Description.........
+
+Public exports for the AudioStream domain package.
+
+===============================================================================
+*/
+
+export * from "./audio-stream";
+export * from "./audio-stream-id";
+export * from "./audio-language";
+export * from "./audio-codec";
+export * from "./audio-channels";
