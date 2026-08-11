5f055c80de8d38467673fe2e90caf12304212f1e
LibraryofLegends <tlorenz.iphone12@gmail.com>
2026-08-11 12:21:28 +0200

    Add files via upload

---
+++ Library Of Legend/index.ts
@@ -0,0 +1,19 @@
+/*
+===============================================================================
+                            PROJECT PHOENIX
+===============================================================================
+Component...........: ApplicationEntry
+Architecture Layer..: Framework
+Module..............: Bootstrap
+Module ID...........: LOL-MOD-BOOT-0001
+LOL-ID..............: LOL-BOOT-0001
+File................: index.ts
+Location............: Library Of Legends/src/
+Version.............: 1.0.0
+Status..............: Core
+Lifecycle...........: Production
+Description.........: Process entry point.
+===============================================================================
+*/
+
+import "./main";
